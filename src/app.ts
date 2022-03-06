c3006ae542e02115bd79cc3e7fc3e4dc44fbf941
learningmode <learningmode151@gmail.com>
2022-03-07 01:01:39 +0530

    different response and routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
-import express from 'express';
+import express,{Request,Response} from 'express';
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    return res.send("Hello world");
+app.use(express.json());
+
+//app.use(express.urlencoded({extended:true}));
+
+app.get("/",(req:Request,res:Response)=>{
+    return res.json({
+        success:true,
+        name:'TomDoesTech'
+    });
+});
+
+app.post('/api/data',(req:Request,res:Response)=>{
+    console.log(req.body);
+
+    return  res.sendStatus(200)
+})
+
+app.all('/api/all',(req:Request,res:Response)=>{
+    return res.sendStatus(200);
 });
 
 app.listen(3000,()=>{
     console.log("Application listening at localhost:3000")
-})
+});
